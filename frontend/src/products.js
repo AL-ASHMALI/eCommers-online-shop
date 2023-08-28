231b10477fb332f2550d9ced725925d82d31fb74
Rabie Al-Ashmali <codeyourfuture@Rabie.local>
2023-08-29 00:20:16 +0100

    Listing the products

---
+++ frontend/src/products.js
@@ -0,0 +1,16 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Airpods Pro 2',
+    image: '/images/airpods.jpg',
+    description: 'Very good headphone',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 229.99,
+    countInStock: 20,
+    rating: 4.5,
+    numReviews: 222,
+  },
+];
+
+export default products;
