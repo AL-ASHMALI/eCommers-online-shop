648180e4dc0ede40dca19ba324b3b32c33cb9e0a
Rabie Al-Ashmali <codeyourfuture@Rabie.local>
2023-09-03 23:40:36 +0100

    creating the model for the data

---
+++ backend/models/userModel.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: 'string',
+      required: true,
+    },
+    email: {
+      type: 'string',
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: 'string',
+      required: true,
+    },
+    isAdmin: {
+      type: 'boolean',
+      required: true,
+      default: false,
+    },
+  },
+  { Timestamps: true }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
