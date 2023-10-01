f05d8fcef3c5260c05f9c01b521ee99e0c3e84ab
Rabie Al-Ashmali <codeyourfuture@Rabie.local>
2023-10-01 21:35:28 +0100

    product controller is added to controll the routes and make the code more organised

--- backend/routes/ProductRoutes.js
+++ backend/routes/ProductRoutes.js
@@ -1,31 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import asyncHandler from '../middleware/asyncHandler.js';
-import Products from '../models/productModel.js';
-import Product from '../models/productModel.js';
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js';
 
-// All Products
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({}); //passing an empty object to get all the products
-    res.json(products);
-  })
-);
-
-//One Product by Id
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      return res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Resource not found');
-    }
-  })
-);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
