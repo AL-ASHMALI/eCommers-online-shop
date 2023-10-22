2c032ea09c24c752bd8c18019de9a8c65ad92f54
Rabie Al-Ashmali <codeyourfuture@Rabie.local>
2023-10-22 15:40:00 +0100

    adding slices for the cart functionality

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -7,7 +7,20 @@ const initialState = localStorage.getItem('cart')
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+      const existItem = state.cartItems.find((y) => y._id === item._id);
+
+      if (existItem) {
+        state.cartItems = state.cartItems.map((y) =>
+          XMLDocument._id === existItem._id ? item : y
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+    },
+  },
 });
 
 export default cartSlice.reducer;
