a68312ff1d28c7ba6e6ffebe5063be93105ad4de
Rabie Al-Ashmali <codeyourfuture@Rabie.local>
2023-10-23 21:44:43 +0100

    removed tax price

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -33,14 +33,9 @@ const cartSlice = createSlice({
       //Calculate the shipping price (if order is above £50 then shipping is free, else £4.99 for shipping)
       state.shippingPrice = addDecimals(state.itemsPrice > 50 ? 0 : 4.99);
 
-      // Calculate tax price (20% tax)
-      state.taxPrice = addDecimals(Number((0.2 * state.itemsPrice).toFixed(2)));
-
       //Calculate total price
-      state.totalPrice = addDecimals(
-        Number(state.itemsPrice) +
-          Number(state.shippingPrice) +
-          Number(state.taxPrice)
+      state.totalPrice = (
+        Number(state.itemsPrice) + Number(state.shippingPrice)
       ).toFixed(2);
 
       localStorage.setItem('cart', JSON.stringify(state));
