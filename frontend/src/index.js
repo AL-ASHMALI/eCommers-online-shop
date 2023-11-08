e6df29cb5d5f06864e122bb85c27a52af693eabb
Rabie Al-Ashmali <codeyourfuture@Rabie.local>
2023-11-08 00:56:06 +0000

    started working on the checkout process / shipping screen added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,6 +18,7 @@ import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,6 +28,7 @@ const router = createBrowserRouter(
       <Route index={true} path='/cart' element={<CartScreen />} />
       <Route index={true} path='/login' element={<LoginScreen />} />
       <Route index={true} path='/register' element={<RegisterScreen />} />
+      <Route index={true} path='/shipping' element={<ShippingScreen />} />
     </Route>
   )
 );
