869ed1240f79f12f7ea9743e990ae3bc24618ee1
Rabie Al-Ashmali <codeyourfuture@Rabie.local>
2023-11-12 20:29:16 +0000

    added private routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -19,6 +19,7 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
+import PrivateRoute from './components/PrivateRoute';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -28,7 +29,10 @@ const router = createBrowserRouter(
       <Route index={true} path='/cart' element={<CartScreen />} />
       <Route index={true} path='/login' element={<LoginScreen />} />
       <Route index={true} path='/register' element={<RegisterScreen />} />
-      <Route index={true} path='/shipping' element={<ShippingScreen />} />
+
+      <Route path='' element={<PrivateRoute />}>
+        <Route index={true} path='/shipping' element={<ShippingScreen />} />
+      </Route>
     </Route>
   )
 );
