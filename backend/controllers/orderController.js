4c054c8335c36839dea88c8156b9e962423e0741
Rabie Al-Ashmali <codeyourfuture@Rabie.local>
2023-11-18 15:44:41 +0000

    payment methods and controllers

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,53 @@
+import asyncHandler from '../middleware/asyncHandler.js';
+import Order from '../models/orderModel.js';
+
+// Description   Create New Order
+// Route         POST /api/orders
+// Access        Private
+const addOrderItems = asyncHandler(async (req, res) => {
+  res.send('Add order items');
+});
+
+// Description   Get orders for logged in user
+// Route         GET /api/orders/myorders
+// Access        Private
+const getMyOrders = asyncHandler(async (req, res) => {
+  res.send('Get my orders');
+});
+
+// Description   Get orders by id
+// Route         GET /api/orders/:id
+// Access        Private
+const getOrdersById = asyncHandler(async (req, res) => {
+  res.send('Get orders by id');
+});
+
+// Description   Update order to paid
+// Route         GET /api/orders/:id/pay
+// Access        Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  res.send('Update order to paid');
+});
+
+// Description   update order to delivered
+// Route         GET /api/orders/:id/deliver
+// Access        Private/Admin
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  res.send('Update order to delivered');
+});
+
+// Description   Get all orders
+// Route         GET /api/orders
+// Access        Private/Admin
+const getOrders = asyncHandler(async (req, res) => {
+  res.send('Get all orders');
+});
+
+export {
+  addOrderItems,
+  getMyOrders,
+  getOrdersById,
+  updateOrderToDelivered,
+  updateOrderToPaid,
+  getOrders,
+};
