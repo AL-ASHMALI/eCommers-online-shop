24d2fa4994c9f49fd753eff905ce46f8aba1809a
Rabie Al-Ashmali <codeyourfuture@Rabie.local>
2023-12-11 00:10:13 +0000

    upload image endpoint and installed Multer

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,51 @@
+import path from 'path';
+import express from 'express';
+import multer from 'multer';
+
+const router = express.Router();
+
+// Setting up the storage location
+const storage = multer.diskStorage({
+  destination(req, res, cb) {
+    cb(null, 'uploads/'); // null is for the error, we don't have an error so we write null
+  },
+  filename(req, res, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}` // file name format
+    );
+  },
+});
+
+// This function filters which file should be uploaded
+function fileFilter(req, file, cd) {
+  const filetypes = /jpg|jpeg|png/;
+  const mimetypes = /image\/jpg|image\/png|image\/webp/;
+
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = mimetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    cb(null, true);
+  } else {
+    cb(new Error('Images only!'), false);
+  }
+}
+
+const upload = multer({ storage, fileFilter });
+const uploadSingleImage = upload.single('image');
+
+router.post('/', (req, res) => {
+  uploadSingleImage(req, res, function (err) {
+    if (err) {
+      return res.status(400).send({ message: err.message });
+    }
+
+    res.status(200).send({
+      message: 'Image uploaded successfully',
+      image: `/${req.file.path}`,
+    });
+  });
+});
+
+export default router;
