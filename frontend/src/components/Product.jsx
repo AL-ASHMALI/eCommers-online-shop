191e3cb4b655c63505675288d9e203152ed4464e
Rabie Al-Ashmali <codeyourfuture@Rabie.local>
2023-12-30 16:23:41 +0000

    design change

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,26 +1,28 @@
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Rating from './Rating';
+import '../assets/styles/index.css';
 
 const Products = ({ product }) => {
   return (
-    <Card className='my-3 p-3 rounded'>
+    <Card className='my-3 custom-card'>
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant='top' />
+        <div className='card-image-container'>
+          <Card.Img src={product.image} variant='top' className='card-image' />
+        </div>
       </Link>
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title className='product-title' as='div'>
+          <Card.Title className='product-title'>
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
-        <Card.Text as='div'>
-          <Rating
-            value={product.rating}
-            text={`${product.numReviews} reviews`}
-          />
-        </Card.Text>
         <Card.Text as='h3'>£{product.price}</Card.Text>
+        <Card.Text>
+          <span className='product-rating'>
+            {product.rating} ({product.numReviews} reviews)
+          </span>
+        </Card.Text>
       </Card.Body>
     </Card>
   );
